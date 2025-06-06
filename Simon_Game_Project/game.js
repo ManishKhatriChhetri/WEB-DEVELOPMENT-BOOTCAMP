430d6ac0ba5c90f64a4e327d4ca891486a821f95
Manish Khatri Chhetri <manish.khatrichhetri@my.utsa.edu>
2025-06-06 10:42:42 -0500

    Simon Game Project: functionality add for mobile devices

--- Simon_Game_Project/game.js
+++ Simon_Game_Project/game.js
@@ -7,7 +7,7 @@ var userClickedPattern= [];
 var level = 0;
 var started = false;
 
-$(document).keydown(
+$(document).on("keydown touchstart", 
     function () {
         if (!started) {
             $("#level-title").text("Level "+level);
